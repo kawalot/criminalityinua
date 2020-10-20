1fdbc9afdeaf577e72f15995a28bd401648363bf
Alexander Peshcherov <kawalot@gmail.com>
2020-10-20 10:51:55 +0300

    Add markers to map

--- src/components/Leafletmap.js
+++ src/components/Leafletmap.js
@@ -23,7 +23,36 @@ class LeafletMap extends React.Component {
   }
 
   render() {
-    console.log(this.props.markers)
+
+    const markerList = this.props.markers.map(function(item, index) {
+
+      const { 
+        address,
+        color,
+        comment,
+        date,
+        icon,
+        latitude,
+        longitude,
+        registry_number, 
+        url, 
+      } = item.node
+
+      const get_coordinates = [latitude, longitude]
+      
+      //console.log(item.node)
+      //console.log(registry_number)
+      return (
+        
+        <Marker key={registry_number} position={get_coordinates} >
+            <Popup>
+                {comment} <br /> <a href={url}>{url}</a>
+            </Popup>
+        </Marker>
+      )
+    })
+
+    //console.log(this.props.markers)
     if (typeof window !== 'undefined') {
       return (
         <Map center={this.props.position} zoom={this.props.zoom}>
@@ -31,11 +60,7 @@ class LeafletMap extends React.Component {
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        {this.props.markerText !== "" &&
-        <Marker position={this.props.position}>
-          <Popup>{this.props.markerText}</Popup>
-        </Marker>
-        }
+        {markerList}
       </Map>
       );
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,19 @@ import Layout from "../components/Layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import LeafletMap from '../components/Leafletmap'
+import { graphql } from 'gatsby'
 
 const IndexPage = ({ data }) => (
-
-
   <Layout>
-      {data.allCases.edges.forEach(element => (console.log(element))) }
     <SEO title="Home" />
     <LeafletMap
-    position={[46.431306, 30.715389]}
-    zoom={11}
-    markers={data.allCases.edges}
+      position={[46.431306, 30.715389]}
+      zoom={11}
+      markers={data.allCases.edges}
     />
-
-
   </Layout>
 )
+
 export const query = graphql`
     query {
       allCases {
